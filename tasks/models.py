5f09616d2a2db79e960e10a5e75d295fbf26aedb
Loay Shubair <loayshubair26@gmail.com>
2026-05-30 22:21:52 +0300

    authentication and user-based task filtering

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class OpenTaskManager(models.Manager):
@@ -10,6 +11,7 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     status = models.CharField(max_length=20, default="open")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     objects = OpenTaskManager()
 
     def __str__(self):
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Task
+
+
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(owner=request.user)
+    return render(request, "tasks/task_list.html", {"tasks": tasks})
+
 
 # Create your views here.
